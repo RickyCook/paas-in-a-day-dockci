RickyCook/paas-in-a-day-dockci

commit c632d38d34717e99e0f2a77503a2e56b07dea7da

Fix for style: empty string is False-ish

dockci/models/build.py
@@ -364,7 +364,7 @@ class Build(Model):  # pylint:disable=too-many-instance-attributes
             data_file_path = os.path.join(*stage.data_file_path())
             with open(data_file_path, 'r') as handle:
                 line = handle.readline().strip()
-                if not line == '':
+                if not line:
                     self.version = line
                     self.save()
 

commit 3842cf8e4774d3bc689fc7d06762a368c1e64b37

Fix logic bug introduced in c632d38d
Test suite would probably have saved me there.
New card: https://trello.com/c/tuxVCP8z

dockci/models/build.py
@@ -364,7 +364,7 @@ class Build(Model):  # pylint:disable=too-many-instance-attributes
             data_file_path = os.path.join(*stage.data_file_path())
             with open(data_file_path, 'r') as handle:
                 line = handle.readline().strip()
-                if not line:
+                if line:
                     self.version = line
                     self.save()
 